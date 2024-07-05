1e82e7ba12115bed16d7740eaf96fd700ea5df63
Static Newman <dmitry.tronin@jetbrains.com>
2024-07-05 15:16:10 +0200

    Add flatten method to MyEnum
    Implemented a flatten method to MyEnum enum. The method attempts to retrieve the successful result, returning an Error if it's not present. This should make handling responses more straightforward.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub mod rpc {
-    pub type RpcResult<T,E = RpcError> = Result<Option<T>, E>;
-    
+    pub type RpcResult<T, E = RpcError> = Result<Option<T>, E>;
+
     #[derive(Debug)]
     pub enum RpcError {
         ConnectionError,
@@ -8,12 +8,24 @@ pub mod rpc {
         DataError,
     }
 }
+
 pub enum MyEnum<T, E = rpc::RpcError> {
-    Ok(rpc::RpcResult<T,E>),
+    Ok(rpc::RpcResult<T, E>),
     Err(E),
 }
 
+impl<T, E: Default> MyEnum<T, E> {
+    pub fn flatten(self) -> Result<T, E> {
+        match self {
+            MyEnum::Ok(Ok(Some(value))) => Ok(value),
+            MyEnum::Ok(_) => Err(Default::default()),
+            MyEnum::Err(e) => Err(e),
+        }
+    }
+}
+
 fn main() {
-    let val = MyEnum::Ok::<String>(Ok(Some("Hello".to_owned())));
+    let val: MyEnum<String, rpc::RpcError> = MyEnum::Ok(Ok(Some("Hello".to_owned())));
     let result = val.flatten();
+    println!("{:?}", result);
 }
